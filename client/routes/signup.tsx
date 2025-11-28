f8b9e4208a0b8d73eebe81a15820ea6dd2103b91
Leif Riksheim <riksheim.leif@gmail.com>
2025-11-28 17:22:35 +0000

    try to use buttons instead

--- client/routes/signup.tsx
+++ client/routes/signup.tsx
@@ -1,8 +1,11 @@
 import { createFileRoute } from "@tanstack/react-router";
 import PageLayout from "../layout/PageLayout/PageLayout";
-import { SignUp } from "@clerk/clerk-react";
+import { SignInButton, SignUp, SignUpButton } from "@clerk/clerk-react";
 import Container from "../primitives/Container/Container";
 import Box from "../primitives/Box/Box";
+import Button from "../primitives/Button/Button";
+import { Text } from "../primitives/Text/Text";
+import { Group } from "../primitives/Group/Group";
 
 export const Route = createFileRoute("/signup")({
   component: Signup,
@@ -14,7 +17,21 @@ function Signup() {
     <PageLayout>
       <Container size="xs">
         <Box py="6xl">
-          <SignUp />
+          <Box pb="4xl">
+            <Text size="xl" weight="bold" align="center" mb="lg">
+              Welcome to Ortus
+            </Text>
+          </Box>
+          <Group direction="row" align="center" justify="center" spacing="md">
+            <SignUpButton>
+              <Button size="lg">Sign up</Button>
+            </SignUpButton>
+            <SignInButton>
+              <Button variant="secondary" size="lg" style={{ marginTop: 16 }}>
+                Sign in
+              </Button>
+            </SignInButton>
+          </Group>
         </Box>
       </Container>
     </PageLayout>
