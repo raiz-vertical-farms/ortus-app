c329fcddd6477d0b45d0bf67fc989ba34b3876ed
Leif Riksheim <riksheim.leif@gmail.com>
2026-04-30 17:44:59 +0100

    update irrigation schedule

--- server/src/config/schedules.ts
+++ server/src/config/schedules.ts
@@ -1,11 +1,11 @@
 export const SCHEDULE_DEFAULTS = {
   light: {
-    minutes_on: 720,  // 12 hours on
+    minutes_on: 720, // 12 hours on
     minutes_off: 720, // 12 hours off
   },
   irrigation: {
-    minutes_on: 2,    // 2 minutes watering
-    minutes_off: 718, // ~12 hours off
+    minutes_on: 60, // 1 hour watering
+    minutes_off: 420, // ~7 hours off (to make 3 cycles/day)
   },
 } as const;
 
